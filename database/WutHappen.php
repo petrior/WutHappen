2d70f42c96610827ff605551fd4e73a995fdc408
petrior <petri.raut@gmail.com>
2014-03-31 21:54:57 +0300

    Added register function and password hashing.

--- database/WutHappen.php
+++ database/WutHappen.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 	// Includes.
 	require_once("./database/dbYhteys.php");
 	
@@ -13,7 +13,7 @@
 		// Constructor.
 		public function __construct()
 		{
-			$this->connectionInfo = getInfo(); // Save connection info to array from dbYhteys.php.
+			$this->connectionInfo = getInfo(); // Save connection info from dbYhteys.php to an array.
 		}
 		
 		// Just for testing... not really needed.
@@ -49,6 +49,54 @@
 		public function endSession()
 		{
 			session_destroy();
+			unset($_SESSION['logged']);
+			unset($_SESSION['user']);
+		}
+		
+		// Register user.
+		public function register($user, $pwd)
+		{
+			// For email.
+			$regExp1 = '/^[a-z0-9\+\-_]+(\.[a-z0-9\+\-_]+)*@[a-z0-9\-]+(\.[a-z0-9\-]+)*\.[a-z]{2,6}$/i';
+			// For password.
+			$regExp2 = '/[A-Z]+/';
+			$regExp3 = '/[0-9]+/';
+			$regExp4 = '/.{8,}/';
+			
+			// Check email.
+			if(preg_match($regExp1, $user) == 0)
+			{
+				echo("Email ei kelpaa!");
+				exit;
+			}
+			
+			// Check password.
+			if(preg_match($regExp2, $pwd) == 0 || preg_match($regExp3, $pwd) == 0 || preg_match($regExp4, $pwd) == 0)
+			{
+				echo("Salasana ei kelpaa!");
+				exit;
+			}
+			
+			// Add salt.
+			$pwd += $this->salt;
+			
+			// Hash the password.
+			$hashedPwd = hash('sha256', $pwd);
+			
+			$sql = "SELECT email FROM wh_users WHERE email='$user';";
+			$STH = @$this->DBH->query($sql);
+			if($STH->rowCount() == 0)
+			{
+				$sql = "INSERT INTO wh_users(admin, password, email) VALUES(
+					0,
+					'$hashedPwd',
+					'$user'
+				);";
+				$STH = @$this->DBH->query($sql);
+			}
+			else {
+				echo("Sähköpostiosoite on jo käytössä.");
+			}
 		}
 		
 		// Login
@@ -63,22 +111,49 @@
 				echo(1);
 			}
 			else {
-				echo(0);
+				echo("Käyttäjätunnus tai salasana virheellinen.");
 			}
 		}
 		
 		// Check if there is a user with specific email and password.
-		// TODO Hash + salt!
-		private function checkUser($user, $pwd) {
+		private function checkUser($user, $pwd)
+		{
+			// Add salt to password.
+			$pwd += $this->salt;
+			
+			// Hash the password.
+			$hashedPwd = hash('sha256', $pwd);
+			
 			$sql = "SELECT * FROM wh_users WHERE email='$user' AND
-			password = '$pwd'";
+			password = '$hashedPwd'";
 			$STH = @$this->DBH->query($sql);
 			if($STH->rowCount() > 0){
 				$row = $STH->fetch();
 				return $row["uid"];
 			} else {
 				return false;
+			}
 		}
-}
+		
+		// Force SSL
+		public function SSLon()
+		{
+			if($_SERVER['HTTPS'] != 'on')
+			{
+				$url = "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+				if(!headers_sent())
+				{
+					header('Location: ' . $url);
+					exit;
+				} else {
+					echo '<script type="text/javascript">';
+					echo 'window.location.href="'.$url.'";';
+					echo '</script>';
+					echo '<noscript>';
+					echo '<meta http-equiv="refresh" content="0;url='.$url.'" />';
+					echo '</noscript>'; exit;
+				}
+			}
+		}
 	}
 ?>
