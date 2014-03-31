3ea0b81b754af502ff9e8b30b6b65302e2355e12
petrior <petri.raut@gmail.com>
2014-03-31 21:53:52 +0300

    Updated test.php

--- test.php
+++ test.php
@@ -1,13 +1,36 @@
-<?php
-	require_once("./database/WutHappen.php");
-	
-	$wutHappen = new WutHappen();
-	
-	var_dump($wutHappen->getConnectionInfo());
-	
-	$wutHappen->dbConnect();
-	
-	$wutHappen->startSession();
-	$wutHappen->login("petri.raut@gmail.com", "salasana");
-	$wutHappen->endSession();
-?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<?php
+			// Tämä tarvitaan tietenkin sivulle...
+			require_once("./database/WutHappen.php");
+			
+			// Eka asia koodissa.
+			$wutHappen = new WutHappen();
+			
+			// Pakotetaan https.
+			$wutHappen->SSLon();
+			
+			// Turha testi.
+			var_dump($wutHappen->getConnectionInfo());
+			
+			// Luo database handlerin. Tarvitaan jos haluaa suorittaa MySQL-komentoja.
+			$wutHappen->dbConnect();
+			
+			// Luodaan sessio.
+			$wutHappen->startSession();
+			
+			// Login funktio on helppo käyttää (sähköposti, salasana).
+			$wutHappen->login("c@d.fi", "Salasana5");
+			
+			// Lopetetaan sessio.
+			$wutHappen->endSession();
+			
+			// Rekisteröidään uusi käyttäjä (sähköposti, salasana).
+			$wutHappen->register("c@d.fi", "Salasana5");
+		?>
+	</body>
+</html>
